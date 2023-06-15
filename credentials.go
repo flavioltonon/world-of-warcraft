6a71cb9991560ef58cd6aa15e99cec0b85e3b09e
Flávio Tonon <flavioltonon@gmail.com>
2023-06-15 20:26:39 -0300

    feat: implement helper function to exchange credentials for a token

--- credentials.go
+++ credentials.go
@@ -4,13 +4,13 @@ import ozzo "github.com/go-ozzo/ozzo-validation/v4"
 
 // Credentials are the credentials required to access the Client functionalities. More info at https://develop.battle.net/access/clients
 type Credentials struct {
-	ClientID string
-	Secret   string
+	ClientID     string
+	ClientSecret string
 }
 
 func (vo Credentials) validate() error {
 	return ozzo.ValidateStruct(&vo,
 		ozzo.Field(&vo.ClientID, ozzo.Required),
-		ozzo.Field(&vo.Secret, ozzo.Required),
+		ozzo.Field(&vo.ClientSecret, ozzo.Required),
 	)
 }
---
+++ token.go
@@ -0,0 +1,48 @@
+package worldofwarcraft
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+const _authenticationURL = "https://oauth.battle.net/token"
+
+// Token is an authentication token required to access Blizzard's APIs
+type Token struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type" example:"bearer"`
+	ExpiresIn   int    `json:"expires_in"`
+	Scope       string `json:"scope"`
+}
+
+// getToken returns a token in exchange for a given set of credentials.
+func getToken(credentials Credentials) (*Token, error) {
+	if err := credentials.validate(); err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest(http.MethodPost, _authenticationURL, strings.NewReader("grant_type=client_credentials"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	request.SetBasicAuth(credentials.ClientID, credentials.ClientSecret)
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to do request: %v", err)
+	}
+
+	defer response.Body.Close()
+
+	var body Token
+
+	if err := json.NewDecoder(response.Body).Decode(&body); err != nil {
+		return nil, fmt.Errorf("failed to decode response body: %v", err)
+	}
+
+	return &body, nil
+}
